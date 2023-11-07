4ba350c5c6dd7277415919ef22e9cc0ce2d1d94b
Divya <divyarathaur666@gmail.com>
2023-11-07 22:14:16 +0530

    added homepage

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Home from "../../pages/Home/Home";
+
+const Layout = () => {
+  return (
+    <>
+    <div className="sidebar-section">
+        <div className="sidebar">
+            <div className="sidebar-toggle-icons">
+            </div>
+        </div>
+        <div className="container">
+        <Home/>
+
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Layout;
